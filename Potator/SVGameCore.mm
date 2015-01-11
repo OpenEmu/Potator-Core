32399eabb40d1cb64c85313796f58aa367327b79
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2015-01-11 18:51:19 +0100

    Update file headers + code clean up.

--- Potator/SVGameCore.mm
+++ Potator/SVGameCore.mm
@@ -1,16 +1,16 @@
 /*
- Copyright (c) 2009, OpenEmu Team
+ Copyright (c) 2015, OpenEmu Team
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- * Neither the name of the OpenEmu Team nor the
- names of its contributors may be used to endorse or promote products
- derived from this software without specific prior written permission.
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -18,10 +18,10 @@
  DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "SVGameCore.h"
@@ -38,8 +38,6 @@
     NSString *romName;
     double sampleRate;
 
-    BOOL paused;
-
     uint8_t *romBuffer;
     size_t   romBufferSize;
 
@@ -51,6 +49,7 @@
 #define SCREEN_HEIGHT 160
 #define SCREEN_WIDTH  160
 #define SCREEN_AREA SCREEN_HEIGHT*SCREEN_WIDTH
+
 @implementation SVGameCore
 
 static __weak SVGameCore *_current;
@@ -61,7 +60,6 @@ static __weak SVGameCore *_current;
     if(self)
     {
         videoBuffer = (uint16_t*)malloc(SCREEN_AREA*2);
-        paused = YES;
 
         romBuffer = NULL;
         romBufferSize = 0;
@@ -72,6 +70,12 @@ static __weak SVGameCore *_current;
     _current = self;
 
     return self;
+}
+
+
+- (void)dealloc
+{
+    free(videoBuffer);
 }
 
 #pragma mark - Input
@@ -112,43 +116,26 @@ static __weak SVGameCore *_current;
     supervision_set_colour_scheme(displayMode);
 }
 
-/*
- extern void supervision_init(void);
- extern void supervision_reset(void);
- extern void supervision_reset_handler(void);
- extern void supervision_done(void);
- extern void supervision_exec(int16 *backbuffer, BOOL bRender);
- extern void supervision_exec2(int16 *backbuffer, BOOL bRender);
- extern void supervision_exec3(int16 *backbuffer, BOOL bRender);
- extern void supervision_exec_fast(int16 *backbuffer, BOOL bRender);
- extern BOOL supervision_load(uint8 *rom, uint32 romSize);
- extern BOOL supervision_update_input(void);
- ---    extern void supervision_set_colour_scheme(COLOR_SCHEME ws_colourScheme);
- extern M6502	*supervision_get6502regs(void);
- extern void supervision_turnSound(BOOL bOn);
-
- extern int	sv_loadState(char *statepath, int id);
- extern int	sv_saveState(char *statepath, int id);
- */
-#pragma mark - Old Stuff
-
-static void audio_callback(int16_t left, int16_t right)
-{
-    GET_CURRENT_AND_RETURN();
-
-    [[current ringBufferAtIndex:0] write:&left maxLength:2];
-    [[current ringBufferAtIndex:0] write:&right maxLength:2];
-}
-
-static size_t audio_batch_callback(const int16_t *data, size_t frames)
-{
-    GET_CURRENT_AND_RETURN(frames);
-
-    [[current ringBufferAtIndex:0] write:data maxLength:frames << 2];
-    return frames;
-}
-
 #pragma mark Exectuion
+
+#pragma mark - Emulation
+- (NSTimeInterval)frameInterval
+{
+    return frameInterval ? frameInterval : 2097152./35112.; // 59.7
+}
+
+- (void)resetEmulation
+{
+    supervision_reset();
+}
+
+- (void)stopEmulation
+{
+    supervision_done();
+
+    [super stopEmulation];
+}
+
 - (void)executeFrame
 {
     [self executeFrameSkippingFrame:NO];
@@ -183,7 +170,7 @@ static size_t audio_batch_callback(const int16_t *data, size_t frames)
     return supervision_load(romBuffer, (uint32_t)romBufferSize);
 }
 
-#pragma mark Video
+#pragma mark - Video
 - (const void *)videoBuffer
 {
     return videoBuffer;
@@ -204,33 +191,6 @@ static size_t audio_batch_callback(const int16_t *data, size_t frames)
     return OEIntSizeMake(1, 1);
 }
 
-- (void)resetEmulation
-{
-    supervision_reset();
-}
-
-- (void)stopEmulation
-{
-    NSString *path = romName;
-    NSString *extensionlessFilename = [[path lastPathComponent] stringByDeletingPathExtension];
-
-    NSString *batterySavesDirectory = [self batterySavesDirectoryPath];
-
-    if([batterySavesDirectory length] != 0)
-    {
-        [[NSFileManager defaultManager] createDirectoryAtPath:batterySavesDirectory withIntermediateDirectories:YES attributes:nil error:NULL];
-    }
-
-    supervision_done();
-
-    [super stopEmulation];
-}
-
-- (void)dealloc
-{
-    free(videoBuffer);
-}
-
 - (GLenum)pixelFormat
 {
     return GL_BGRA;
@@ -238,7 +198,6 @@ static size_t audio_batch_callback(const int16_t *data, size_t frames)
 
 - (GLenum)pixelType
 {
-    //   return GL_UNSIGNED_SHORT_5_5_5_1;
     return GL_UNSIGNED_SHORT_1_5_5_5_REV;
 }
 
@@ -247,14 +206,10 @@ static size_t audio_batch_callback(const int16_t *data, size_t frames)
     return GL_RGB5;
 }
 
+#pragma mark - Audio
 - (double)audioSampleRate
 {
     return sampleRate ? sampleRate : 48000;
-}
-
-- (NSTimeInterval)frameInterval
-{
-    return frameInterval ? frameInterval : 2097152./35112.; // 59.7
 }
 
 - (NSUInteger)channelCount
@@ -262,6 +217,7 @@ static size_t audio_batch_callback(const int16_t *data, size_t frames)
     return 2;
 }
 
+#pragma mark - Save States
 - (void)saveStateToFileAtPath:(NSString *)fileName completionHandler:(void (^)(BOOL, NSError *))block
 {
     const char * path = [fileName cStringUsingEncoding:NSUTF8StringEncoding];
